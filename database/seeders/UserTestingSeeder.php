ed2eb3a46a460433c23d8e4a80c39dc3c2d78c78
karin morais <andradkarin@gmail.com>
2023-08-29 18:19:22 -0300

    Added a seeder for testing

---
+++ database/seeders/UserTestingSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserTestingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::insert([
+            [
+                'name' => 'Robert',
+                'email' => fake()->unique()->safeEmail()
+            ],
+            [
+                'name' => 'Paul',
+                'email' => fake()->unique()->safeEmail()
+            ],
+            [
+                'name' => 'Lucas',
+                'email' => fake()->unique()->safeEmail()
+            ],
+            [
+                'name' => 'George',
+                'email' => fake()->unique()->safeEmail()
+            ],
+            [
+                'name' => 'Leonard',
+                'email' => fake()->unique()->safeEmail()
+            ],
+        ]);
+    }
+}
