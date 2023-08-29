f01250135bb17932105f6c913ec58e2ba2d48614
karin morais <andradkarin@gmail.com>
2023-08-29 16:58:29 -0300

    Create user controller and first route

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    public function mostExpensiveOrder() {
+
+        $return = User::select('users.name', DB::raw('max(orders.total_amount) as mostExpensivePurchase'))
+            ->join('orders', 'users.id', '=', 'orders.user_id')
+            ->groupBy('users.id')
+            ->orderBy('mostExpensivePurchase', 'desc')
+            ->get();
+
+        return response()->json(['users' => $return]);
+    }
+}
