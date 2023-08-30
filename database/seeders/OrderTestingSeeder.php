325dd1e576566d1db3bb14ead0049cf6f6b827f7
karin morais <andradkarin@gmail.com>
2023-08-30 09:25:39 -0300

    Created seeder for orders

---
+++ database/seeders/OrderTestingSeeder.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Order;
+use Illuminate\Database\Seeder;
+
+class OrderTestingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Order::insert($this->get_insert_array_of_user1_with_all_products());
+        Order::insert($this->get_insert_array_for_other_users());
+    }
+
+    private function get_insert_array_for_other_users() {
+        return [
+            [
+                'user_id' => 2,
+                'product_id' => 1,
+                'quantity' => 1,
+                'total_amount' => 5000
+            ],
+            [
+                'user_id' => 2,
+                'product_id' => 2,
+                'quantity' => 5,
+                'total_amount' => 7502.5
+            ],
+            [
+                'user_id' => 3,
+                'product_id' => 1,
+                'quantity' => 3,
+                'total_amount' => 15000
+            ],
+            [
+                'user_id' => 3,
+                'product_id' => 3,
+                'quantity' => 2,
+                'total_amount' => 1001.98
+            ]
+        ];
+    }
+
+    private function get_insert_array_of_user1_with_all_products() {
+        return [
+            [
+                'user_id' => 1,
+                'product_id' => 1,
+                'quantity' => 2,
+                'total_amount' => 10000
+            ],
+            [
+                'user_id' => 1,
+                'product_id' => 2,
+                'quantity' => 3,
+                'total_amount' => 4501.5
+            ],
+            [
+                'user_id' => 1,
+                'product_id' => 3,
+                'quantity' => 3,
+                'total_amount' => 1502.97
+            ]
+        ];
+    }
+}
