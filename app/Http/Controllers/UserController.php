a5d8f53e5c9eb18e0526ec9362e37b4698c9f5c4
karin morais <andradkarin@gmail.com>
2023-08-30 09:52:11 -0300

    Added function to get users who have purchased all products

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,12 +10,21 @@ class UserController extends Controller
 {
     public function mostExpensiveOrder() {
 
-        $expensiveOrders = User::select('users.name', DB::raw('max(orders.total_amount) as mostExpensivePurchase'))
+        $expensiveOrders = User::select('users.name', DB::raw('MAX(orders.total_amount) as mostExpensivePurchase'))
             ->join('orders', 'users.id', '=', 'orders.user_id')
             ->groupBy('users.id')
             ->orderBy('mostExpensivePurchase', 'desc')
             ->get();
 
         return response()->json(['users' => $expensiveOrders]);
+    }
+
+    public function usersWhoHavePurchasedAllProducts() {
+        $users = User::select('users.name')
+            ->join('orders', 'users.id', '=', 'orders.user_id')
+            ->join('products', 'orders.product_id', '=', 'products.id')
+            ->groupBy('users.id')
+            ->havingRaw('COUNT(DISTINCT products.id) = (select COUNT(id) FROM products)')
+            ->get();
     }
 }
