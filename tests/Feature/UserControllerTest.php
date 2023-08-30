ea11a72f4c3f0e3255349e5e58a517a5c728d994
karin morais <andradkarin@gmail.com>
2023-08-30 09:26:20 -0300

    Added test for database and endpoint1

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -2,10 +2,13 @@
 
 namespace Tests\Feature;
 
+use Database\Seeders\OrderTestingSeeder;
+use Database\Seeders\ProductTestingSeeder;
 use Database\Seeders\UserTestingSeeder;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class UserControllerTest extends TestCase
@@ -16,15 +19,32 @@ class UserControllerTest extends TestCase
     public function setUp(): void
     {
         parent::setUp();
-        $this->seed(UserTestingSeeder::class);
+        $this->seed([UserTestingSeeder::class, ProductTestingSeeder::class, OrderTestingSeeder::class]);
     }
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+
+    public function test_is_database_set_correctly(): void
+    {
+        $this->assertDatabaseCount('users', 3);
+        $this->assertDatabaseCount('products', 3);
+        $this->assertDatabaseCount('orders', 7);
+    }
+
+    public function test_assert_query_for_most_expensive_order_from_all_users(): void
     {
         $response = $this->get('/api/users/order/most-expensive');
 
+        $response->assertJson(fn (AssertableJson $json) =>
+            $json->has('users', 3)
+                ->where('users.0.mostExpensivePurchase', 15000)
+                ->where('users.0.name', 'Lucas')
+                ->where('users.1.mostExpensivePurchase', 10000)
+                ->where('users.1.name', 'Robert')
+                ->where('users.2.mostExpensivePurchase', 7502.5)
+                ->where('users.2.name', 'Paul')
+        );
         $response->assertStatus(200);
     }
 }
